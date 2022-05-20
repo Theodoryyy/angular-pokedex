cf35f9786d4bce584f003a74205d5ab45a9fbb17
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2022-05-20 18:29:37 -0300

    Criação da pagina do pokemon

--- src/app/routes/route-pokemon/route-pokemon.component.ts
+++ src/app/routes/route-pokemon/route-pokemon.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ServPokeapiService } from 'src/app/services/serv-pokeapi/serv-pokeapi.service';
 
 @Component({
   selector: 'app-route-pokemon',
   templateUrl: './route-pokemon.component.html',
-  styleUrls: ['./route-pokemon.component.scss']
+  styleUrls: ['./route-pokemon.component.scss'],
 })
 export class RoutePokemonComponent implements OnInit {
+  public pokemon: any;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private servPokeapi: ServPokeapiService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.getPokemon('https://pokeapi.co/api/v2/pokemon/' + params['id']);
+    });
   }
 
+  private getPokemon(endPoint: string) {
+    this.servPokeapi.getData(endPoint, (res: any) => {
+      console.log(res);
+      this.pokemon = res;
+    });
+  }
 }
